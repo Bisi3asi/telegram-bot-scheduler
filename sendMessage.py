705b201f58c5d2fab4cfd42ce4f4cff3889d1ef1
Bisi3asi <kueeng8008@naver.com>
2025-07-08 12:48:23 +0900

    feat: 데일리 메시지 전송 성공/실패여부 로깅 기능 추가

--- sendMessage.py
+++ sendMessage.py
@@ -3,6 +3,9 @@ import os
 
 message = "ㅈㅇㅇㅊㅇㄴㄷ"
 
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 payload = {
     "chat_id": CHAT_ID,
@@ -10,4 +13,16 @@ payload = {
 }
 
 response = requests.post(url, data=payload)
-print("전송 완료" if response.status_code == 200 else f"에러: {response.text}")
+
+# 현재 시간 문자열
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# 결과 메시지 작성
+if response.status_code == 200:
+    log_message = f"[{now}] ✅ SEND MESSAGE SUCCESS  : '{message}'"
+else:
+    log_message = f"[{now}] ❌ SEND MESSAGE FAILED   : {response.text}"
+
+# log.txt에 append (누적 기록)
+with open("log.txt", "a", encoding="utf-8") as log_file:
+    log_file.write(log_message + "\n")
