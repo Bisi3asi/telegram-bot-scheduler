4d8c01235e01605a5815bc739a6c15085a04b3bd
Bisi3asi <kueeng8008@naver.com>
2025-07-09 09:30:56 +0900

    fix, feat: datetime 오류 수정, kst 기준 분기, 금요일 스페셜 메시지

--- sendMessage.py
+++ sendMessage.py
@@ -1,28 +1,37 @@
 import requests
 import datetime
 import os
+import pytz
 
-message = "ㅈㅇㅇㅊㅇㄴㄷ"
+dailyMessage = "ㅈㅇㅇㅊㅇㄴㄷ~"
+fridayMessage = "ㅈㅇㄱㅇㅇㅇㄴㄷ~"
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
+
+# 로컬 시간대 설정
+kst = pytz.timezone("Asia/Seoul")
+now_kst = datetime.datetime.now(kst)
+
+if now_kst.weekday() == 4:
+    message = fridayMessage
+else :
+    message = dailyMessage
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 payload = {
     "chat_id": CHAT_ID,
     "text": message
 }
-
 response = requests.post(url, data=payload)
 
-# 현재 시간 문자열
-now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+now_str = now_kst.strftime("%Y-%m-%d %H:%M:%S")
 
 # 결과 메시지 작성
 if response.status_code == 200:
-    log_message = f"[{now}] ✅ SEND MESSAGE SUCCESS  : '{message}'"
+    log_message = f"[{now_str}] ✅ SEND MESSAGE SUCCESS  : '{message}'"
 else:
-    log_message = f"[{now}] ❌ SEND MESSAGE FAILED   : {response.text}"
+    log_message = f"[{now_str}] ❌ SEND MESSAGE FAILED   : {response.text}"
 
 # log.txt에 append (누적 기록)
 with open("log.txt", "a", encoding="utf-8") as log_file:
